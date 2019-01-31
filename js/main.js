9614b837a287d4efae3103d84d32ed8146d9b03b
kunglin <1025288517@qq.com>
2019-01-31 11:25:36 +0800

    修改
    进一步压缩图片，修改分辨率，增加随机祝福语。公司标题增加动画

--- js/main.js
+++ js/main.js
@@ -14,4 +14,15 @@ $(function () {
             audio.pause(); //暂停
         }
     });
+
+    randomBlessing();
 })
+
+function randomBlessing() {
+    var blessing1 = ["猪事顺利", "猪圆玉润", "猪笼入水", "万事如意", "幸福安康", "一帆风顺", "生意兴隆", "大吉大利", "心想事成", "五福临门 "];
+    var blessing2 = ["八面来财", "青春永猪", "百尺竿头", "吉祥如意", "金玉满堂", "福如东海", "飞黄腾达", "年年有余", "六六大顺", "荣华富贵 "];
+    var text1 = blessing1[Math.floor(Math.random() * 10)];
+    var text2 = blessing2[Math.floor(Math.random() * 10)];
+    $("#blessing1").html(text1);
+    $("#blessing2").html(text2);
+}
